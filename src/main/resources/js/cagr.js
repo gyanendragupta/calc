ca021afad5db8ebf3851cbb18b8ae9d09fd2a1e4
gkgupta <gkgupta@5CG6191Y7Z.ctl.intranet>
2017-02-14 18:53:16 +0530

    Added

---
+++ src/main/resources/js/cagr.js
@@ -0,0 +1,46 @@
+function calcCAGR(){
+	var table = document.getElementById("myTableData");
+	var rowCount = table.rows.length;
+	var outdate = new Date(document.getElementById("asOnDate").value);
+	var cagr = document.getElementById("cagr").value	
+	var total=0;	
+	for (var i=1; i < rowCount; i++){
+		var indateId = "inDate"+i;		
+		var invalId = "inVal"+i;
+		var indate = new Date(document.getElementById(indateId).value);		
+		var principle = document.getElementById(invalId).value;
+		var days = (outdate - indate)/86400000;
+		//alert(days);
+		//alert(Math.pow((1+(cagr/100)),days/365));
+		var compoundVal = ((Math.pow(1+(cagr/100),days/365))* principle);
+		total = total+compoundVal;
+		//alert(total);
+		//alert("i is "+i);	
+	}
+	document.getElementById("totalVal").value=total;
+}
+
+function clear(obj){
+
+}
+
+function addRow() {		 
+	var table = document.getElementById("myTableData");
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+    
+    var inDateId="inDate"+rowCount;
+    var inValId="inVal"+rowCount;
+
+    row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
+    row.insertCell(1).innerHTML= '<input id='+inDateId+' type="text" value="2015-10-25">';
+    row.insertCell(2).innerHTML= '<input id='+inValId+' type="text" value="">';
+}
+
+function deleteRow(obj) {
+	 
+	var index = obj.parentNode.parentNode.rowIndex;
+	var table = document.getElementById("myTableData");
+	table.deleteRow(index);
+	
+}
